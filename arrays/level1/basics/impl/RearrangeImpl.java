959907663059e6912bf15f96eb512880d6c6a62c
Satyam Saw <satyamkumarsaw30@gmail.com>
2026-06-16 01:10:49 +0530

    implemented move zeros to end

--- arrays/level1/basics/impl/RearrangeImpl.java
+++ arrays/level1/basics/impl/RearrangeImpl.java
@@ -2,7 +2,20 @@ package arrays.level1.basics.impl;
 
 import arrays.level1.basics.Rearrange;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RearrangeImpl extends Rearrange {
+
+    public static void main(String[] args) {
+
+        RearrangeImpl rearrange = new RearrangeImpl();
+        int[] arr = new int[]{0, 1, 0, 3, 12};
+
+        rearrange.moveZeros(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
 
     @Override
     public void reverse(int[] arr) {
@@ -12,8 +25,28 @@ public class RearrangeImpl extends Rearrange {
 
     @Override
     public void moveZeros(int[] arr) {
-        // TODO [A05] Move zeros to the end, keep order of non-zeros.
-        throw new UnsupportedOperationException("TODO: moveZeros");
+//        int insertPos = 0;
+//        for (int i = 0; i < arr.length; i++) if (arr[i] != 0) arr[insertPos++] = arr[i];
+//        while (insertPos < arr.length) arr[insertPos++] = 0;
+        int left = 0;
+        int right = 0;
+        while (right < arr.length) {
+            if (arr[right] != 0) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
+
+    private void swapItems(int[] arr, int idx1, int idx2) {
+        var printLine = "idx1: " + idx1 + " idx2: " + idx2;
+        System.out.println(printLine);
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
     }
 
     @Override
